721f4dee391b515e937e1443f5f860d809e0534f
chriswasser <christian_wassermann@web.de>
2022-12-08 12:32:08 +0100

    Clean up solution for Day08

--- Day08/main.py
+++ Day08/main.py
@@ -2,6 +2,7 @@
 
 import fileinput
 import math
+from typing import Callable, Generator, Iterable, TypeVar
 
 import numpy as np
 from rich import print
@@ -9,56 +10,62 @@ from rich.traceback import install
 
 install(show_locals=True)
 
+T = TypeVar("T")
+
 
 def process_part1(lines: list[str]) -> str:
-    forest = np.array([[int(tree) for tree in line] for line in lines], dtype=np.int64)
+    forest = np.array([[int(tree) for tree in line] for line in lines], dtype=np.int8)
+
     visible = 0
     for (i, j) in np.ndindex(forest.shape):
         tree = forest[i, j]
-        visible += np.all(forest[:i, j] < tree) or np.all(forest[i+1:, j] < tree) or np.all(forest[i, :j] < tree) or np.all(forest[i, j+1:] < tree)
+        visible += (
+            False
+            or np.all(forest[:i, j] < tree)
+            or np.all(forest[i + 1 :, j] < tree)
+            or np.all(forest[i, :j] < tree)
+            or np.all(forest[i, j + 1 :] < tree)
+        )
     return str(visible)
 
 
+def until(predicate: Callable[[T], bool], iterable: Iterable[T]) -> Generator[T, None, None]:
+    for item in iterable:
+        yield item
+        if predicate(item):
+            break
+
+
+# alternative implementation of count_until which composes generators instead of performing a manual loop
+def count_until2(predicate: Callable[[T], bool], iterable: Iterable[T]) -> int:
+    iterator = iter(iterable)
+    iterator = until(predicate, iterator)
+    iterator = map(lambda _: 1, iterator)
+    return sum(iterator)
+
+
+def count_until(predicate: Callable[[T], bool], iterable: Iterable[T]) -> int:
+    count = 0
+    for item in iterable:
+        count += 1
+        if predicate(item):
+            break
+    return count
+
+
 def process_part2(lines: list[str]) -> str:
-    forest = np.array([[int(tree) for tree in line] for line in lines], dtype=np.int64)
+    forest = np.array([[int(tree) for tree in line] for line in lines], dtype=np.int8)
 
     result = 0
     for (i, j) in np.ndindex(forest.shape):
         tree = forest[i, j]
 
-        totals = []
-
-        total = 0
-        for a in forest[:i, j][::-1]:
-            if a >= tree:
-                total += 1
-                break
-            total += 1
-        totals.append(total)
-
-        total = 0
-        for a in forest[i+1:, j]:
-            if a >= tree:
-                total += 1
-                break
-            total += 1
-        totals.append(total)
-
-        total = 0
-        for a in forest[i, :j][::-1]:
-            if a >= tree:
-                total += 1
-                break
-            total += 1
-        totals.append(total)
-
-        total = 0
-        for a in forest[i, j+1:]:
-            if a >= tree:
-                total += 1
-                break
-            total += 1
-        totals.append(total)
+        totals = [
+            count_until(lambda other: other >= tree, forest[:i, j][::-1]),
+            count_until(lambda other: other >= tree, forest[i + 1 :, j]),
+            count_until(lambda other: other >= tree, forest[i, :j][::-1]),
+            count_until(lambda other: other >= tree, forest[i, j + 1 :]),
+        ]
 
         result = max(result, math.prod(totals))
     return str(result)
